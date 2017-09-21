872816715be94aafd953d369095a163d8d0cf949
hblok <github@hblok.net>
2017-09-21 13:36:46 +0200

    Just getting started

---
+++ init_eclipse_prj.sh
@@ -0,0 +1,1 @@
+mvn -DdownloadSources=true -DoutputDirectory=target/eclipse-classes -Declipse.workspace=/home/havardrb/user_files/test-results-analyzer-plugin  eclipse:eclipse eclipse:configure-workspace
