a6ab2b70715d3b07d7673c152ea7ccc117f51b27
hblok <gitmail@hblok.net>
2017-09-27 05:18:52 +0200

    spaces for tabs

--- src/main/webapp/js/testresult.js
+++ src/main/webapp/js/testresult.js
@@ -26,8 +26,8 @@ function searchTests(){
 }
 
 function clearedFilter(rows) {
-	rows.filter("[custom='show']").removeClass("row-hidden");
-	rows.filter("[custom='hide']").addClass("row-hidden");
+    rows.filter("[custom='show']").removeClass("row-hidden");
+    rows.filter("[custom='hide']").addClass("row-hidden");
 }
 
 function applyFilter(rows, filter) {
@@ -35,9 +35,9 @@ function applyFilter(rows, filter) {
         var dom = $j(row);
         var name = dom.find(".name")[0];
         var nameDom = $j(name);
-		if (nameDom.hasClass("name-heading")) {
-			return;
-		}
+        if (nameDom.hasClass("name-heading")) {
+            return;
+        }
 
         var matchName = nameDom.text().toLowerCase();
         if (matchName.indexOf(filter) == -1) {
@@ -50,137 +50,137 @@ function applyFilter(rows, filter) {
 }
 
 function createTable(data) {
-	var buildIds = data["builds"];
-	var results = data["results"];
-	
-	var table = $j(".test-history-table");
-	
-	createHeader(table, buildIds);
-	createRows(table, results, 0);
+    var buildIds = data["builds"];
+    var results = data["results"];
+
+    var table = $j(".test-history-table");
+
+    createHeader(table, buildIds);
+    createRows(table, results, 0);
 }
 
 function createHeader(parentDom, buildIds) {
-	var rowDom = createRow(parentDom, "heading", -1, "Package / Class / Test");
-	
-	for(var i = 0; i < buildIds.length; i++) {
-		var cell = $j("<div>")
-			.addClass("table-cell")
-			.text(buildIds[i])
-			.appendTo(rowDom);
-	}		
+    var rowDom = createRow(parentDom, "heading", -1, "Package / Class / Test");
+
+    for(var i = 0; i < buildIds.length; i++) {
+        var cell = $j("<div>")
+            .addClass("table-cell")
+            .text(buildIds[i])
+            .appendTo(rowDom);
+    }
 }
 
 var LEVELNAME = ["package", "class", "test"];
 
 function createRows(parentDom, results, level) {
-	var children = results["children"];
-	var names = Object.keys(children);
-	for (var i = 0; i < names.length; i++) {
-		var name = names[i];
-		var subResults = children[name];
-		
-		var rowDom = createRow(parentDom, LEVELNAME[level], level, name);
-		//createSpacingRow(parentDom, LEVELNAME[level], level);
-		createTestResults(rowDom, subResults["testResults"]);
-		if (level < 2) {
-			createRows(parentDom, subResults, level + 1);
-		}
-		createSpacingRow(parentDom, LEVELNAME[level], level);
-	}
+    var children = results["children"];
+    var names = Object.keys(children);
+    for (var i = 0; i < names.length; i++) {
+        var name = names[i];
+        var subResults = children[name];
+
+        var rowDom = createRow(parentDom, LEVELNAME[level], level, name);
+        //createSpacingRow(parentDom, LEVELNAME[level], level);
+        createTestResults(rowDom, subResults["testResults"]);
+        if (level < 2) {
+            createRows(parentDom, subResults, level + 1);
+        }
+        createSpacingRow(parentDom, LEVELNAME[level], level);
+    }
 }
 
 function createSpacingRow(parentDom, rowType, level) {
-	var rowDom = $j("<div>")
-	.addClass("table-row")
-	.addClass("row-spacing-" + rowType)
-	.attr("level", level);
+    var rowDom = $j("<div>")
+    .addClass("table-row")
+    .addClass("row-spacing-" + rowType)
+    .attr("level", level);
 
-	if (level > 0) {
-		rowDom.addClass("row-hidden");
-		rowDom.attr("custom", "hide");
-	} else {
-		rowDom.attr("custom", "show");
-	}
+    if (level > 0) {
+        rowDom.addClass("row-hidden");
+        rowDom.attr("custom", "hide");
+    } else {
+        rowDom.attr("custom", "show");
+    }
 
-	parentDom.append(rowDom);
+    parentDom.append(rowDom);
 }
 
 var STATUSCSSMAP = {
-	"P": "passed",
-	"F": "failed",
-	"S": "skipped",
-	"/": "na"
+    "P": "passed",
+    "F": "failed",
+    "S": "skipped",
+    "/": "na"
 };
 
 function createTestResults(parentDom, results) {
-	for (var i = 0; i < results.length; i++) {
-		var status = results[i];
-		var cell = $j("<div>")
-			.addClass("table-cell")
-			.addClass("build-result")
-			.addClass(STATUSCSSMAP[status])
-			.text(status)
-			.appendTo(parentDom);
-	}
+    for (var i = 0; i < results.length; i++) {
+        var status = results[i];
+        var cell = $j("<div>")
+            .addClass("table-cell")
+            .addClass("build-result")
+            .addClass(STATUSCSSMAP[status])
+            .text(status)
+            .appendTo(parentDom);
+    }
 }
 
 function createRow(parentDom, rowType, level, name) {
-	var rowDom = $j("<div>")
-		.addClass("table-row")
-		.addClass("table-row-" + rowType)
-		.attr("level", level);
+    var rowDom = $j("<div>")
+        .addClass("table-row")
+        .addClass("table-row-" + rowType)
+        .attr("level", level);
 
-	if (level > 0) {
-		rowDom.addClass("row-hidden");
-		rowDom.attr("custom", "hide");
-	} else {
-		rowDom.attr("custom", "show");
-	}
-	
-	var nameDom = $j("<div>")
-		.addClass("name")
-		.addClass("name-" + rowType)
-		.text(name)
-		.appendTo(rowDom);
-		
-	if (rowType == "package" || rowType == "class" ) {
-		var expand = $j("<div>")
-			.addClass("icon")
-			.addClass("icon-plus-sign")
-			.prependTo(nameDom)
-			.click(rowDom, handleExpand);
-	}
-		
-	parentDom.append(rowDom);
-	
-	return rowDom;
+    if (level > 0) {
+        rowDom.addClass("row-hidden");
+        rowDom.attr("custom", "hide");
+    } else {
+        rowDom.attr("custom", "show");
+    }
+
+    var nameDom = $j("<div>")
+        .addClass("name")
+        .addClass("name-" + rowType)
+        .text(name)
+        .appendTo(rowDom);
+
+    if (rowType == "package" || rowType == "class" ) {
+        var expand = $j("<div>")
+            .addClass("icon")
+            .addClass("icon-plus-sign")
+            .prependTo(nameDom)
+            .click(rowDom, handleExpand);
+    }
+
+    parentDom.append(rowDom);
+
+    return rowDom;
 }
 
 function handleExpand(src) {
-	var row = $j(src.data);
-	var icon = row.find(".icon");
-	var expand = icon.hasClass("icon-plus-sign");
-	var level = parseInt(row.attr("level"));
+    var row = $j(src.data);
+    var icon = row.find(".icon");
+    var expand = icon.hasClass("icon-plus-sign");
+    var level = parseInt(row.attr("level"));
 
-	icon.removeClass(expand ? "icon-plus-sign" : "icon-minus-sign");
-	icon.addClass(expand ? "icon-minus-sign" : "icon-plus-sign");
+    icon.removeClass(expand ? "icon-plus-sign" : "icon-minus-sign");
+    icon.addClass(expand ? "icon-minus-sign" : "icon-plus-sign");
 
-	var next = row.next();
-	var nextLevel = parseInt(next.attr("level"));
-	while (next.length > 0 && nextLevel > level) {
-		if (nextLevel == level + 1 && expand) {
-			next.removeClass("row-hidden");
-			next.attr("custom", "show");
-		} else if (nextLevel > level && !expand) {
-			next.addClass("row-hidden");
-			next.attr("custom", "hide");
-			var icons = next.find(".icon");
-			icons.removeClass("icon-minus-sign");
-			icons.addClass("icon-plus-sign");
-		}
-		next = next.next();
-		nextLevel = parseInt(next.attr("level"));
-	}
+    var next = row.next();
+    var nextLevel = parseInt(next.attr("level"));
+    while (next.length > 0 && nextLevel > level) {
+        if (nextLevel == level + 1 && expand) {
+            next.removeClass("row-hidden");
+            next.attr("custom", "show");
+        } else if (nextLevel > level && !expand) {
+            next.addClass("row-hidden");
+            next.attr("custom", "hide");
+            var icons = next.find(".icon");
+            icons.removeClass("icon-minus-sign");
+            icons.addClass("icon-plus-sign");
+        }
+        next = next.next();
+        nextLevel = parseInt(next.attr("level"));
+    }
 }
 
 function getUserConfig(){
